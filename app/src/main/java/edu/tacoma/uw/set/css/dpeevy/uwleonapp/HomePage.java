b5f3dd32c570ec875e3ba7bb287db6107b14bd80
miguel <ramosmiguel40@gmail.com>
2023-08-01 11:54:45 -0700

    Added Fonts and Branding Assets to res folder. Added Homepage navigation buttons.

--- app/src/main/java/edu/tacoma/uw/set/css/dpeevy/uwleonapp/HomePage.java
+++ app/src/main/java/edu/tacoma/uw/set/css/dpeevy/uwleonapp/HomePage.java
@@ -2,10 +2,9 @@ package edu.tacoma.uw.set.css.dpeevy.uwleonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 
 public class HomePage extends AppCompatActivity {
 
@@ -18,20 +17,6 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Button b = findViewById(R.id.about_button);
-        setButtonListener(b, AboutLeonPage.class);
-
-        b = findViewById(R.id.navigation_button);
-        setButtonListener(b, NavigationPage.class);
     }
 
-    private void setButtonListener(Button button, Class<?> cls) {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(), cls);
-                startActivity(i);
-            }
-        });
-    }
 }
